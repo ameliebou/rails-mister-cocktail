cd288563d2a4723ebbaafa357442cad20aab9814
Amelie Boudier <amelie.boudier01@gmail.com>
2018-11-19 11:07:29 +0100

    changed nabar

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,20 @@
 require 'open-uri'
-
+Cocktail.destroy_all
 Ingredient.destroy_all
 
 url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
 ingredients = JSON.parse(open(url).read)
 
+array = []
+
 ingredients['drinks'].each do |ing|
-  Ingredient.create!(name: ing['strIngredient1'])
+  array << ing['strIngredient1']
 end
+
+array << 'Tonic water'
+
+array.sort!
+
+array.each { |el| Ingredient.create!(name: el) }
 
 puts 'ingredients seed done!'
