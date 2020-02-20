1c2a940759afa3d29036a5e4c6d872cbd2953d3d
Amelie Boudier <amelie.boudier01@gmail.com>
2020-02-20 11:58:26 +0100

    unnested destroy method of dose

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get root to: 'cocktails#index'
   resources :cocktails, only: [:index, :show, :new, :create] do
-    resources :doses, only: [:new, :create, :destroy]
+    resources :doses, only: [:new, :create]
   end
+
+  resources :doses, only: :destroy
 end
